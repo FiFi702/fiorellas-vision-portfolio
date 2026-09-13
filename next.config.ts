1002940e1d5de79deaf92a81cc27b44a08b34309
FiFi702 <fiorella.mule.h63953@mcast.edu.mt>
2026-09-13 20:25:28 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/fiorellas-vision-portfolio",
+  
   trailingSlash: true,
   images: {
     unoptimized: true,
