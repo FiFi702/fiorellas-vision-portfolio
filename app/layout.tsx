0dbb4f1cb0d19589d6ba5a0dc291ac7d1befe7e2
FiFi702 <fiorella.mule.h63953@mcast.edu.mt>
2026-09-13 23:26:30 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export const metadata: Metadata = {
     "codex-preview": "development",
   },
   icons: {
-    icon: "/fiorellas-vision-portfolio/favicon.png",
-    shortcut: "/fiorellas-vision-portfolio/favicon.png",
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
   },
 };
 
